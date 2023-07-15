1770470dc7d18448f7c7a66aa807f564ecd755c9
yamsroun <yamsroun@gmail.com>
2023-07-15 10:09:59 +0900

    스프링 부트 테스트 코드 (1)

---
+++ src/test/java/jocture/testcode/TestcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package jocture.testcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
